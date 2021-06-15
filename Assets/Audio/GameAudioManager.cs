d4ea67f6463fcc5f39e1f2c7ee0f68477e647c75
Ivan <45919772+ivanMuzyka144@users.noreply.github.com>
2021-06-15 20:36:31 +0200

    Add option system

--- Assets/Audio/GameAudioManager.cs
+++ Assets/Audio/GameAudioManager.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class GameAudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private AudioClip gameMusic;
+    [SerializeField] private AudioClip buttonClickSound;
+    [SerializeField] private AudioClip softClick;
+    [SerializeField] private AudioClip blink1;
+    [SerializeField] private AudioClip blink2;
+    [SerializeField] private AudioClip blink3;
+    [SerializeField] private AudioClip dragSound;
+    [SerializeField] private AudioClip dropSound;
+    [Space(10)]
+    [SerializeField] private AudioSource musicAudioSource;
+    [SerializeField] private AudioSource soundAudioSource;
+    private void Start()
     {
-        
+        musicAudioSource.PlayOneShot(gameMusic);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayButtonClickMusic()
     {
-        
+        soundAudioSource.PlayOneShot(buttonClickSound);
     }
+
+    public void PlaySoftButtonClickMusic()
+    {
+        soundAudioSource.PlayOneShot(softClick);
+    }
+
 }
--- Assets/Scripts/MainMenu/GameMenuManager.cs
+++ Assets/Scripts/MainMenu/GameMenuManager.cs
@@ -1,11 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameMenuManager : MonoBehaviour
 {
     [SerializeField] private SelectionManager selectionManager;
+    [SerializeField] private GameAudioManager gameAudioManager;
+    [Space(10)]
+    [SerializeField] private Options options;
+    [Space(10)]
+    [SerializeField] private Toggle musicToggle;
+    [SerializeField] private Toggle soundToggle;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider soundSlider;
+    [SerializeField] private Dropdown languageDropdown;
+    [SerializeField] private GameObject optionsWindow;
+    [SerializeField] private AudioMixer audioMixer;
+    private void Start()
+    {
+        audioMixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("musicVolume"));
+        audioMixer.SetFloat("SoundVolume", PlayerPrefs.GetFloat("soundVolume"));
+        if (PlayerPrefs.GetInt("shouldPlayMusic") == 0)
+        {
+            audioMixer.SetFloat("MusicVolume", -80);
+        }
+        if (PlayerPrefs.GetInt("shouldPlaySound") == 0)
+        {
+            audioMixer.SetFloat("SoundVolume", -80);
+        }
+    }
     public void OnMenuClicked()
     {
         //LeanTransition leanTrasition = GameObject.Find("LeanTransition").GetComponent<LeanTransition>();
@@ -16,13 +42,87 @@ public class GameMenuManager : MonoBehaviour
 
     public void OnExitClicked()
     {
+        gameAudioManager.PlayButtonClickMusic();
         Time.timeScale = 1;
         SceneManager.LoadScene("StartScene");
     }
 
     public void OnAllMenuClosed()
     {
+        gameAudioManager.PlayButtonClickMusic();
         Time.timeScale = 1;
         selectionManager.Unpause();
     }
+
+    public void OnOptionsClicked()
+    {
+        gameAudioManager.PlayButtonClickMusic();
+        optionsWindow.SetActive(true);
+        musicToggle.isOn = PlayerPrefs.GetInt("shouldPlayMusic") == 1;
+        soundToggle.isOn = PlayerPrefs.GetInt("shouldPlaySound") == 1;
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        soundSlider.value = PlayerPrefs.GetFloat("soundVolume");
+    }
+
+    public void OnOptionsWindowClosed()
+    {
+        gameAudioManager.PlayButtonClickMusic();
+        optionsWindow.SetActive(false);
+    }
+
+    public void OnMusicToggleChanged(bool result)
+    {
+        options.ChangeCanPlayMusic(result);
+        if (PlayerPrefs.GetInt("shouldPlayMusic") == 0)
+        {
+            audioMixer.SetFloat("MusicVolume", -80);
+        }
+        else
+        {
+            audioMixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("musicVolume"));
+        }
+        gameAudioManager.PlayButtonClickMusic();
+    }
+
+    public void OnSoundToggleChanged(bool result)
+    {
+        options.ChangeCanPlaySound(result);
+        if (PlayerPrefs.GetInt("shouldPlaySound") == 0)
+        {
+            audioMixer.SetFloat("SoundVolume", -80);
+        }
+        else
+        {
+            audioMixer.SetFloat("SoundVolume", PlayerPrefs.GetFloat("soundVolume"));
+        }
+        gameAudioManager.PlayButtonClickMusic();
+    }
+
+    public void OnMusicSliderChanged()
+    {
+        gameAudioManager.PlaySoftButtonClickMusic();
+        options.ChangeMusicVolume(musicSlider.value);
+        if (PlayerPrefs.GetInt("shouldPlayMusic") == 1)
+        {
+            audioMixer.SetFloat("MusicVolume", musicSlider.value);
+        }
+        PlayerPrefs.SetFloat("musicVolume", musicSlider.value);
+    }
+    public void OnSoundSliderChanged()
+    {
+        gameAudioManager.PlaySoftButtonClickMusic();
+        options.ChangeSoundVolume(soundSlider.value);
+        if (PlayerPrefs.GetInt("shouldPlaySound") == 1)
+        {
+            audioMixer.SetFloat("SoundVolume", soundSlider.value);
+        }
+        PlayerPrefs.SetFloat("soundVolume", soundSlider.value);
+    }
+
+    public void OnLanguageDropdownChanged()
+    {
+        gameAudioManager.PlayButtonClickMusic();
+        Debug.Log("" + languageDropdown.value);
+    }
+    
 }
