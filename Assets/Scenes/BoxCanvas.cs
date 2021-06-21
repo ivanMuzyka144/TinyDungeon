bb7d6ec00d74d02fbff3bf8aedc713ce1537aca4
Ivan <45919772+ivanMuzyka144@users.noreply.github.com>
2021-06-21 13:59:49 +0200

    Update ui canvas

--- Assets/Scenes/BoxCanvas.cs
+++ Assets/Scenes/BoxCanvas.cs
@@ -1,11 +1,77 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class BoxCanvas : MonoBehaviour
+public class BoxCanvas : MonoBehaviour, ISelectable
 {
+    public Button button;
+    public Toggle toggle;
+
+    bool enableForEdit = true;
+
+    public GameObject GetObject()
+    {
+        return gameObject;
+    }
+
     public void InvokeUIElement()
     {
+        if(button!=null)
+            button.onClick.Invoke();
+        if (toggle != null)
+        {
+            Debug.Log("ffff");
+            if (toggle.isOn)
+            {
+                if (enableForEdit)
+                {
+                    toggle.isOn = false;
+                    StartCoroutine(BlockingCoroutine());
+                }
+            }
+            else
+            {
+                if (enableForEdit)
+                {
+                    toggle.isOn = true;
+                    StartCoroutine(BlockingCoroutine());
+                }
 
+            }
+        }
+            
+    }
+
+    IEnumerator BlockingCoroutine()
+    {
+
+        enableForEdit = false;
+        yield return new WaitForSeconds(0.5f);
+        enableForEdit = true;
+    }
+
+    public bool IsSelected()
+    {
+        return true;
+    }
+
+    public void MakeSelectionAction()
+    {
+        
+    }
+
+    public void OnDeselected()
+    {
+        
+    }
+
+    public void OnSelected()
+    {
+       
+    }
+
+    public void OnStillSelected()
+    {
+        
     }
 }
